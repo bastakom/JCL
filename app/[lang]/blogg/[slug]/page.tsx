861d75cac994776c4cdc4ec81c1086bba4f7fe4e
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-02-14 09:30:03 +0100

    korr styling blogg

--- app/[lang]/blogg/[slug]/page.tsx
+++ app/[lang]/blogg/[slug]/page.tsx
@@ -33,7 +33,7 @@ const page = async ({ params }: { params: { slug: string } }) => {
   const hasContent = content?.content?.content?.length > 0;
   return (
     <div className="pt-28 pb-10 lg:py-44">
-      <div className="container">
+      <div className="container lg:w-[70%]">
         {hasImage ? (
           <div className="relative h-[300px] lg:h-[500px]">
             <Image
@@ -50,7 +50,7 @@ const page = async ({ params }: { params: { slug: string } }) => {
         )}
 
         <div className="lg:pr-20 flex flex-col mt-10 gap-5">
-          <h1 className="my-5 lg:my-0">{name}</h1>
+          <h1 className="my-5 lg:my-0 lg:leading-[1.4]">{name}</h1>
           {hasContent ? (
             <span>{render(content.content)}</span>
           ) : (
