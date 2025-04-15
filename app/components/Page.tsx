98e84daec6e5ba9b2c442e67908638788ab55ab9
andjan3 <150336624+andjan3@users.noreply.github.com>
2025-04-15 14:30:05 +0200

    fix: translatable links now working correctly

--- app/components/Page.tsx
+++ app/components/Page.tsx
@@ -1,11 +1,15 @@
 import { storyblokEditable, StoryblokComponent } from "@storyblok/react/rsc";
 
-const Page = ({ blok }: any) => (
+const Page = ({ blok, locale }: any) => (
   <div {...storyblokEditable(blok)}>
     {blok &&
       Array.isArray(blok.body) &&
       blok.body.map((nestedBlok: any, index: number) => (
-        <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+        <StoryblokComponent
+          blok={nestedBlok}
+          key={nestedBlok._uid}
+          locale={locale}
+        />
       ))}
   </div>
 );
